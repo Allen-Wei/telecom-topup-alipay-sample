4cdc7de3c449bf282beb5288a569b0d3a7d461ba
Alan <alan.wei@live.com>
2019-05-20 23:04:44 +0800

    init

---
+++ src/test/java/net/alanwei/telecom/TopupApplicationTests.java
@@ -0,0 +1,16 @@
+package net.alanwei.telecom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
